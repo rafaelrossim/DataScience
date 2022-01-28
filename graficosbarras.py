b10d5e7abac782005c2fd836d423bfd7a7f9333b
RafaelRossim <58317726+rafaelrossim@users.noreply.github.com>
2022-01-28 15:54:58 -0300

    criando graficos em barras

--- graficosbarras.py
+++ graficosbarras.py
@@ -2,9 +2,9 @@
 import matplotlib.pyplot as plt
 
 #variaveis
-x=[1, 2, 5]
-y=[3, 4, 7]
-titulo = "Graficos em linhas"
+x=[1, 2, 3, 4, 5]
+y=[3, 4, 7, 1, 0]
+titulo = "Graficos em barras"
 eixox = "eixo X"
 eixoy = "eixo Y"
 
@@ -13,8 +13,8 @@ plt.title(titulo)
 plt.xlabel(eixox)
 plt.ylabel(eixoy)
 
-#utlizando o plot para criar graficos em linha
-plt.plot(x, y)
+#utlizando o bar para criar graficos em barras
+plt.bar(x, y)
 
 #mostrando o grafico
 plt.show()
